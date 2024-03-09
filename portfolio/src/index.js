48e2f3d0c68796ef5632b02c9880015f9dcc3828
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-09 14:27:31 -0500

    Implementing concepts of classes

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -6,5 +6,18 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
 
+    }
+
+    speak() {
+        console.log('I am ', this.name, 'and I am', this.age, 'years old');
+    }
+}
+
+const animal1 = new Animal('Simba', 3);
+animal1.speak();
 
