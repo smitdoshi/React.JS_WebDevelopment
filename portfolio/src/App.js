4831ae296e4665468db39a9795aae6ef1749cc5b
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-09 04:04:16 -0500

    Added app component and rendering it through default root

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component }from 'react';
+
+
+class App extends Component {
+    render() {
+         return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Smit. I'm a software engineer.</p>
+                <p>I'm always looking forwared to working on meaningful projects.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+ 
