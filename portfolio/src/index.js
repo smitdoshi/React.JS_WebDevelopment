c504ec0df2dc86e016889bc51bddeb5dd76b8c21
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-09 14:40:13 -0500

    Implemented concept of classes and how inheritance works as a sample

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -21,3 +21,28 @@ class Animal {
 const animal1 = new Animal('Simba', 3);
 animal1.speak();
 
+console.log(animal1); // animal class instance
+
+class Lion extends Animal {
+    constructor(name, age, furColor, speed){
+        /* We do not have to do 
+            this.name = name; 
+            this.age = age;
+            Since that code already exists in Animal Class 
+        */
+       // Inorder to invoke the constructor of parent class we use SUPER keyword'
+       super(name, age);  // Invoke the constructor of parent class.
+       this.furColor = furColor;
+       this.speed = speed;
+    }
+    // Classes can have it's unqiue method with it's new properties
+    roar(){
+        console.log('ROOOAR ! I have',this.furColor,'fur, and I can run', this.speed, 'miles an hours !');
+    }
+}
+
+const lion1 = new Lion('Mufasa', 20, 'golden', 25);
+lion1.speak();
+lion1.roar();
+
+console.log(lion1);
