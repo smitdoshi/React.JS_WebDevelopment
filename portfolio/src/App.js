ed768221a19245e5c17393ea11c7c155df6da47b
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-10 14:00:02 -0400

    Removed the uncommented lines and code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -21,43 +21,7 @@ class App extends Component {
         // this.toggleDisplayBio = () => {...}
     }
 
-
-    // constructor(){
-    //     super(); // Calling super in order invoke the constructor of parent class, React Component in our case.
-    //     this.state = { displayBio: false };
-
-    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-    // }
-    
-    
-
-    // Adding toggle method to combine above readMore and showLess method.
-    // toggleDisplayBio => will set the displayBio to it's opposite value whatever currently it may be.
-    /**
-     *  toggleDisplayBio(){
-            this.setState({ displayBio: !this.state.displayBio });
-        }
-     */
-    
-
-
     render() {
-        // to make block of JSX use ();
-        // As soon as JSX sees null value it will skip to render bio part
-
-        // Now the below bio = whole div block we can render bio within the render method by using {}
-        // {} = Allows you render variables within actual JSX and display them.
-        
-        /*
-            if(!this.state.displayBio){
-            bio = null;
-        }*/
-
-        // Instead of above code section we can have ternary operator to check if displayBio is true and toggle then
-        // Added a condition to check displayBio value
-        // Since we are using const bio variable only once we can take whole bio section and render it inline JSX
-
-        // Now we will be giving control to the users to display Bio or Not, by implementing SetState Concepts
         return (
             <div>
                 <h1>Hello!</h1>
