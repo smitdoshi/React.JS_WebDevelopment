b37c669a50047dc826c8e228556b971e09d6aadc
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-10 11:17:02 -0400

    Added inline JSX condition and removed cont variable since we are using it only once

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import React, { Component }from 'react';
 class App extends Component {
     constructor(){
         super(); // Calling super in order invoke the constructor of parent class, React Component in our case.
-        this.state = { displayBio: false };
+        this.state = { displayBio: true };
     }
 
 
@@ -24,21 +24,20 @@ class App extends Component {
         // Added a condition to check displayBio value
         // Since we are using const bio variable only once we can take whole bio section and render it inline JSX
 
-        const bio = this.state.displayBio ? (
-            <div>
-                    <p>I live in Orlando, and code every day.</p>
-                    <p> My favourite language is Javasript, and I am learning React.js</p>
-                    <p> Beisde coding, I love being outdoor and travelling.</p>
-            </div>
-        ) : null;
-        
-
          return (
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Smit. I'm a software engineer.</p>
                 <p>I'm always looking forwared to working on meaningful projects.</p>
-            {bio}
+            {
+                this.state.displayBio ? (
+                    <div>
+                            <p>I live in Orlando, and code every day.</p>
+                            <p> My favourite language is Javasript, and I am learning React.js</p>
+                            <p> Beisde coding, I love being outdoor and travelling.</p>
+                    </div>
+                ) : null
+            }
             </div>
         );
     }
