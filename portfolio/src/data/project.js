bd43536e42763a43d030f616b123d2e1f2ecaec6
SMIT N DOSHI <smitdoshi007@gmail.com>
2024-03-10 20:03:12 -0400

    Added dummy data as JSON format

--- portfolio/src/data/project.js
+++ portfolio/src/data/project.js
@@ -2,4 +2,33 @@
  import project2 from '../assets/project2.png';
  import project3 from '../assets/project3.png';
 
+
+ // Here notice PROJECTS is all in uppercase it's called SCREAM_CASE syntax
+ // In the convention for global values in a javascript project is to use SCREAM_CASE syntax.
  
+ const PROJECTS = [
+    {
+        id: 1,
+        title: 'Example React Application',
+        description: 'A React App that I built, involving JS and Core Web Dev Concepts!',
+        link:'https://github.com/smitdoshi/React.JS_WebDevelopment/tree/c3c17c42f455fe77076832a954821d6ca226279c/portfolio',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'My API',
+        description: 'A React API that I built from scratch with GET and POST requests!',
+        link:'https://github.com/smitdoshi/React.JS_WebDevelopment/tree/c3c17c42f455fe77076832a954821d6ca226279c/portfolio',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Operating Systems Final Project',
+        description: 'My unique final project for my university Operating Systems course.',
+        link:'https://github.com/smitdoshi/React.JS_WebDevelopment/tree/c3c17c42f455fe77076832a954821d6ca226279c/portfolio',
+        image: project3
+    }
+ ] ; 
+
+ 
+ export default PROJECTS;
